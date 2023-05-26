306bcb26f56d91d3c9d642ba37941e8c7e04898d
Martin <cheyuan0621@icloud.com>
2023-05-26 13:28:50 +0800

    0526php_VOTE

--- back/add_vote.php
+++ back/add_vote.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>新增主題</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/jquery-3.7.0.min.js"></script>
 </head>
 <body>
 <main>
 
     <h1>新增主題</h1>
-    <form action="./api/add_vote.php" method="post">
+    <form action="../api/add_vote.php" method="post">
         <div>
             <label for="subject">主題說明：</label>
             <input type="text" name="subject" id="subject" class="subject-input">
@@ -29,6 +31,11 @@
             <label for="type">單複選：</label>
             <input type="radio" name="type" value="1">單選&nbsp;&nbsp;
             <input type="radio" name="type" value="2">複選
+        </div>
+        <div>
+            <label for="type">是否公開：</label>
+            <input type="radio" name="login" value="0">是&nbsp;&nbsp;
+            <input type="radio" name="login" value="1">否
         </div>
         <hr>
     <div class="options">        
--- backend.php
+++ backend.php
@@ -1,4 +1,16 @@
-<?php include_once "db.php";?>
+<?php include_once "db.php";
+$do='';
+if(isset($_GET['do'])){
+    $do=$_GET['do'];
+}else{
+    if(isset($_SESSION['pr'])){
+        $do=$_SESSION['pr'];
+    }else{
+        $do="error";
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,11 +27,27 @@
     <a href="index.php">網站首頁</a>
     <a href="backend.php">管理首頁</a>
     <a href="./api/logout.php">登出</a>
-    <nav>
-        <a href='./backend.php?do=add_vote'>新增投票</a>     
-        <a href='./backend.php?do=query_vote'>會員管理</a>
-        <a href='./backend.php?do=query_vote'>投票明細管理</a>
-    </nav>
+    <?php 
+    switch($_SESSION['pr']){
+        case "super":
+
+        break;
+        case "admin":
+            echo "<nav>";
+            echo "    <a href='./backend.php?do=add_vote'>新增投票</a>";
+            echo "    <a href='./backend.php?do=query_vote'>投票明細管理</a>";
+            echo "</nav>";
+        break;
+        case "member":        
+            echo "<nav>";
+            echo "    <a href='./backend.php?do=edit_self'>修改個人資料</a>";
+            echo "    <a href='./backend.php?do=vote_history'>投票紀錄查詢</a>";
+            echo "</nav>";
+        break;
+    }
+
+    ?>
+
 </header>
 <main>
 <?php
@@ -32,14 +60,14 @@
  } */
 
 //$do=(isset($_GET['do']))?$_GET['do']:'topic_list';
-$do=$_GET['do']??'topic_list';
+
 
 $file="./back/".$do.".php";
 
 if(file_exists($file)){
     include $file;
 }else{
-    include "./back/topic_list.php";
+    include "./back/error.php";
 }
 
 //include (file_exists($file))?$file:"./back/topic_list.php";
